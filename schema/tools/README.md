fe2dceec59127e56edfa69a1eb39adfccb88d645
David Raznick <kindly@gmail.com>
2017-03-09 08:40:35 +0000

    Add tool to produce fake and blank data

---
+++ schema/tools/README.md
@@ -0,0 +1,21 @@
+To install dependencies for these tools, cd into this directory an run
+
+```
+npm install .
+```
+
+
+Data Faker
+----------
+
+To generate fake data into a file run:
+
+```
+node data-faker.js > myfakedatafile.json
+```
+
+To generate a datafile with all the fields blank:
+
+```
+node data-faker.js -b > myblankdatafile.json
+```
---
+++ schema/tools/data-faker.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var jsf = require('json-schema-faker');
+var schema
+
+program
+    .option('-b --blank', 'Blank data')
+    .parse(process.argv);
+
+
+
+var jsonschema = 
+    JSON.parse(
+        require('fs').readFileSync(
+            require('path').resolve(
+                __dirname, 
+                '../beneficial-ownership-statements.json'),
+            'utf8'));
+
+
+
+jsf.format('URI', function(gen, jsonschema) {
+    return gen.randexp('^http://[A-Za-z0-9]+\\.com$');
+});
+
+jsf.option({
+    alwaysFakeOptionals: true
+});
+
+
+
+var modifySchema = function(schema) {
+    var change_definition = function(def, prop) {
+
+        delete def.title
+        delete def.description
+        if (prop === 'id') {
+            def['minLength'] = 20;
+            def['type'] = 'string';
+        }
+        if (def.type === 'object') {
+            modifySchema(def)
+        }
+        if (def.format === 'date') {
+            def.format = 'date-time'
+        }
+        if (def.format && def.format === 'uri') {
+            def.format = 'URI'
+        }
+        if (program.blank) {
+          if (def.type === 'array') {
+            def.minItems = 1
+            def.maxItems = 1
+          }
+          if (def.type === 'string') {
+            delete def.format
+            def.enum = [""]
+          }
+          if (def.type === 'number') {
+            def.enum = [0]
+          }
+          if (def.type === 'boolean') {
+            def.enum = [false]
+          }
+        }
+    }
+    var prop;
+    var definition;
+
+    for (prop in schema.properties) {
+      var def = schema.properties[prop]
+      change_definition(def, prop)
+    }
+
+    if (schema.definitions) {
+        for (definition in schema.definitions) {
+            if (schema.definitions[definition].hasOwnProperty('properties')) {
+              modifySchema(schema.definitions[definition])
+            } else {
+              change_definition(schema.definitions[definition])
+            }
+        }
+    }
+
+    return schema
+}
+
+jsonschema = modifySchema(jsonschema);
+sample = jsf(jsonschema);
+console.log(JSON.stringify(sample, null, 2))
