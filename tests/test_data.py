ed6c1e8f50a926c1eb5744e526bf7a6bd579d29b
Ben Webb <ben.webb@opendataservices.coop>
2018-03-20 13:19:58 +0000

    Move bods validation functions into seperate file

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,43 +1,11 @@
 import os
 import json
-from jsonschema import validate, ValidationError, RefResolver, FormatChecker
 import pytest
+from jsonschema import validate, ValidationError
 
-
-this_dir = os.path.dirname(os.path.realpath(__file__))
-absolute_path_to_schema_dir = this_dir + '/../schema/'
-resolver = RefResolver('file://' + absolute_path_to_schema_dir + '/', None)
-format_checker = FormatChecker()
-
-
-class MissingStatementTypeError(ValueError):
-    pass
-
-
-class UnknownStatementTypeError(ValueError):
-    pass
-
-
-def bods_validate_statement(statement):
-    schemas = {
-        'entityStatement': 'entity-statement.json',
-        'personStatement': 'person-statement.json',
-        'beneficialOwnershipStatement': 'beneficial-ownership-statement.json',
-    }
-    statement_type = statement.get('statementType')
-    if not statement_type:
-        raise MissingStatementTypeError()
-    schema_path = schemas.get(statement_type)
-    if not schema_path:
-        raise UnknownStatementTypeError('Unknown statementType {}'.format(statement.get('statementType')))
-    with open(os.path.join(absolute_path_to_schema_dir, schema_path)) as f:
-        schema = json.load(f)
-    validate(statement, schema, resolver=resolver, format_checker=format_checker)
-
-
-def bods_validate_package(statement_list):
-    for statement in statement_list:
-        bods_validate_statement(statement)
+from bods_validate import this_dir, format_checker, absolute_path_to_schema_dir, resolver
+from bods_validate import MissingStatementTypeError
+from bods_validate import bods_validate_package, bods_validate_statement
 
 
 @pytest.mark.parametrize('json_path', [
