8a93fe20b0ac9884d9ae655d6f1bb22003a1027b
Jack Lord <jack.lord@opendataservices.coop>
2018-06-07 13:29:08 +0100

    required fields for loose validation and invalid test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -145,12 +145,17 @@ def test_invalid_statement_json_iter_errors(json_path, expected_errors):
     ], {
         "'statementType' is a required property",
     }),
+    (None, [
+        'data/entity-statement/invalid/entity-statement-loose-validation-without-required-fields.json',
+    ], {
+       "{'id': '00335', 'register': 'Jebel Ali Free Zone'} is not valid under any of the given schemas",
+    }),
     ('data/bods-package/fails-secondary-validation/bods-package-missing-entity-statement.json', None, {
         "subject/entitiy/describedByStatement '1dc0e987-5c57-4a1c-b3ad-61353b66a9b7' does not match any known entities"
     }),
     ('data/bods-package/fails-secondary-validation/bods-package-incorrect-ordering.json', None, {
         "interestedParty/person/describedByStatement '019a93f1-e470-42e9-957b-03559861b2e2' does not match any known persons"
-    }),
+    })
 ])
 def test_invalid_package_json_iter_errors(json_path, json_paths, expected_errors):
     if json_path:
