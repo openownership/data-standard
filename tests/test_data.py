e1155a5ecfe276d1a38dfb717a767a9c96dbae47
Jack Lord <jack.lord@opendataservices.coop>
2018-06-13 21:55:42 +0100

    add linking annotations example to tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -27,6 +27,7 @@ def test_valid_statement_json(json_path):
     'data/bods-package/valid/valid-bods-package.json',
     'data/bods-package/valid/valid-bods-package-entity-owning-entity.json',
     'data/bods-package/valid/valid-bods-package-annotations.json',
+    'data/bods-package/valid/valid-bods-package-linking-annotations.json',
     '../examples/flat-serialisation/gb-coh-bods-package.json',
 ])
 def test_valid_package_json(json_path):
@@ -135,6 +136,7 @@ def test_invalid_statement_json_iter_errors(json_path, expected_errors):
 @pytest.mark.parametrize(('json_path', 'json_paths', 'expected_errors'), [
     ('data/bods-package/valid/valid-bods-package.json', None, set()),
     ('data/bods-package/valid/valid-bods-package-annotations.json', None, set()),
+    ('data/bods-package/valid/valid-bods-package-linking-annotations.json', None, set()),
     (None, [
         'data/entity-statement/valid/valid-entity-statement.json',
         'data/person-statement/valid/valid-person-statement.json',
