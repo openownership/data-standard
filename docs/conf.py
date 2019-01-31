e8d27e79f0c744a1fc095e7a97cfe74a3237a60b
rhiaro <amy@rhiaro.co.uk>
2019-01-31 18:49:41 +0200

    Configure gettext

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,9 @@ release = '0.1'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
+locale_dirs = ['locale/']
+gettext_compact = False
+gettext_uuid = True
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
