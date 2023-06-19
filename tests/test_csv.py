09af93a192003694d9bf639e6bc7e6c0b94e2934
rhiaro <amy@rhiaro.co.uk>
2023-06-19 12:23:07 +0100

    Tests: remove invalid_csv test as it doesn't actually do anything (see https://github.com/open-contracting/jscc/issues/9)

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -6,7 +6,6 @@ from io import StringIO
 
 import pytest
 from jscc.schema import is_codelist
-from jscc.testing.checks import get_invalid_csv_files
 from jscc.testing.filesystem import walk_csv_data
 from jscc.testing.util import warn_and_assert
 from jsonschema import FormatChecker
@@ -22,11 +21,6 @@ def formatwarning(message, category, filename, lineno, line=None):
 
 warnings.formatwarning = formatwarning
 pytestmark = pytest.mark.filterwarnings('always')
-
-
-def test_csv_valid():
-    warn_and_assert(get_invalid_csv_files(), '{0} is not valid CSV: {1}',
-                    'CSV files are invalid. See warnings below.')
 
 
 def test_valid():
