70c3fd9aee3920b17d62f913e4b5733d0b7ca5e2
rhiaro <amy@rhiaro.co.uk>
2024-02-16 16:58:12 +0000

    Tests: test codelists
    Updates to codelist-schema so JSON schema validation can do most
    of the work.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ import pytest
 from pathlib import Path
 from jsonschema import FormatChecker
 from jsonschema.validators import Draft202012Validator
-from jscc.schema import is_json_schema
-from jscc.testing.filesystem import walk_json_data
+from jscc.schema import is_json_schema, is_codelist
+from jscc.testing.filesystem import walk_json_data, walk_csv_data
 from referencing import Registry, Resource
 from referencing.jsonschema import DRAFT202012
 
@@ -26,11 +26,23 @@ def file_id(path):
     return path.name
 
 
+def codelist_id(codelist_data):
+    # codelist data is (path, name, text, fieldnames, rows) from codelist_paths
+    return codelist_data[1]
+
+
 def schema_paths():
     here = os.path.dirname(os.path.realpath(__file__))
     schema_dir = os.path.join(here, "..", "schema")
     schema_paths = [(path, name, data) for path, name, _, data in walk_json_data(top=schema_dir) if is_json_schema(data)]
     return schema_paths
+
+
+def codelist_paths():
+    here = os.path.dirname(os.path.realpath(__file__))
+    codelists_dir = os.path.join(here, "..", "schema", "codelists")
+    codelist_paths = [(path, name, text, fieldnames, rows) for path, name, text, fieldnames, rows in walk_csv_data(top=codelists_dir) if is_codelist(fieldnames)]
+    return codelist_paths
 
 
 def schema_registry():
@@ -95,6 +107,20 @@ def bods_validator():
 
 
 @pytest.fixture
+def codelist_validator():
+    """
+    This sets up and returns a validator for the codelists schema.
+    """
+    # Get the registry
+    registry = schema_registry()
+
+    # Make the validator
+    codelist_schema = registry.contents("urn:codelists")
+    codelist_validator = Draft202012Validator(codelist_schema, registry=registry, format_checker=FormatChecker())
+    return codelist_validator
+
+
+@pytest.fixture
 def bods_json(request):
     """
     Returns JSON from a file path.
@@ -108,21 +134,33 @@ def bods_json(request):
 
 
 @pytest.fixture
+def codelist_json(request):
+    """
+    Returns JSON version of a codelist from a CSV file path.
+    This is automatically called on all params for tests which take
+    `codelist_json` as a parameter with indirect=True, and the output passed to
+    the test.
+    """
+    codelist = request.param # (path, name, text, fieldnames, rows)
+    return codelist[4]
+
+
+@pytest.fixture
 def get_valid_data():
     """
     Returns a simple valid BODS statement.
     """
     data = [{
-            "statementId": "abcdefgkdddddddddddddddddddddddshdkjfkjdkjf",
-            "declarationSubject": "xyz",
-            "recordId": "123",
-            "recordType": "entity",
-            "recordDetails": {
-                "entityType": "unknownEntity",
-                "isComponent": False,
-                "publicationDetails": {}
-            }
-        }]
+        "statementId": "abcdefgkdddddddddddddddddddddddshdkjfkjdkjf",
+        "declarationSubject": "xyz",
+        "recordId": "123",
+        "recordType": "entity",
+        "recordDetails": {
+            "entityType": "unknownEntity",
+            "isComponent": False,
+            "publicationDetails": {}
+        }
+    }]
     return data
 
 
@@ -132,8 +170,8 @@ def get_invalid_data():
     Returns a simple invalid BODS statement.
     """
     data = [{
-            "declarationSubject": "xyz",
-            "recordId": "123",
-            "recordType": "entity"
-        }]
+        "declarationSubject": "xyz",
+        "recordId": "123",
+        "recordType": "entity"
+    }]
     return data
