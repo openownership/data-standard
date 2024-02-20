bf369642ddddf944d72fc79c86e2149d47314aef
rhiaro <amy@rhiaro.co.uk>
2024-02-20 10:39:40 +0000

    Tests: setup schema validation tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,7 +2,19 @@ import os
 import pytest
 from conftest import get_json_files, file_id
 
+"""
+Data tests
+
+Checks that the BODS Schema validates data according to our
+expectations.
+
+These tests should be run whenever changes to the schema affect
+the validation requirements. New requirements can be tested by
+adding new test data files to the /data directory.
+"""
+
 valid_bods_statements = get_json_files(os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "v0.4", "valid-statements"))
+invalid_bods_statements = get_json_files(os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "v0.4", "invalid-statements"))
 
 
 @pytest.mark.parametrize("bods_json", valid_bods_statements, ids=file_id, indirect=True)
@@ -18,6 +30,21 @@ def test_valid_files(bods_validator, bods_json):
             print(error.schema_path)
 
     assert is_valid
+
+
+@pytest.mark.parametrize("bods_json", invalid_bods_statements, ids=file_id, indirect=True)
+def test_invalid_files(bods_validator, bods_json):
+    is_valid = bods_validator.is_valid(bods_json)
+
+    if not is_valid:
+        errors = bods_validator.iter_errors(bods_json)
+        for error in errors:
+            print(error.message)
+            print(error.path)
+            print(error.schema_path)
+
+    assert not is_valid
+    # todo: assert correct error message and field path
 
 
 def test_valid_data(bods_validator, get_valid_data):
