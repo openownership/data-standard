7234d018b18e9370a733007a8d3269fbe2a0ad5a
rhiaro <amy@rhiaro.co.uk>
2024-02-27 17:57:53 +0000

    Tests: Tidy up, add comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,172 @@ from referencing import Registry, Resource
 from referencing.jsonschema import DRAFT202012
 
 
+"""
+The fixtures in this file are automatically discovered by pytest without
+needing to import them in the test files.
+"""
+
+
+@pytest.fixture
+def schema_dir():
+    return get_schema_dir()
+
+
+@pytest.fixture
+def codelists_dir():
+    return os.path.join(get_schema_dir(), 'codelists')
+
+
+@pytest.fixture
+def examples_dir():
+    return get_examples_dir()
+
+
+@pytest.fixture
+def schema_from_registry(request):
+    registry = schema_registry()
+    return registry.contents(request.param)
+
+
+@pytest.fixture
+def schema_validator():
+    """
+    This sets up and returns a 2020-12 validator, against which the BODS
+    schema can be checked. The BODS schema files are loaded into the
+    registry so $refs can be resolved.
+    """
+
+    registry = schema_registry()
+
+    # Get meta schema
+    here = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(here, 'schema', 'meta-schema.json')) as fp:
+        metaschema = json.load(fp)
+
+    validator = Draft202012Validator(metaschema, registry=registry, format_checker=FormatChecker())
+
+    return validator
+
+
+@pytest.fixture
+def bods_validator():
+    """
+    This sets up and returns the validator, using the statement.json
+    schema as the primary schema against which data is validated.
+    """
+
+    # Get the registry
+    registry = schema_registry()
+
+    # Make the validator
+    statement_schema = registry.contents("urn:statement")
+    bods_validator = Draft202012Validator(statement_schema, registry=registry, format_checker=FormatChecker())
+    return bods_validator
+
+
+@pytest.fixture
+def codelist_validator():
+    """
+    This sets up and returns a validator for the codelists schema.
+    """
+    # Get the registry
+    registry = schema_registry()
+
+    # Make the validator
+    codelist_schema = registry.contents("urn:codelists")
+    codelist_validator = Draft202012Validator(codelist_schema, registry=registry, format_checker=FormatChecker())
+    return codelist_validator
+
+
+@pytest.fixture
+def bods_json(request):
+    """
+    Returns JSON from a file path.
+    This is automatically called on all params for tests which take
+    `bods_json` as a parameter with indirect=True, and the output passed to
+    the test.
+    """
+    fp = request.param
+    bods = json.loads(fp.read_text())
+    return bods
+
+
+@pytest.fixture
+def codelist_json(request):
+    """
+    Returns JSON version of a codelist from a CSV file path.
+    This is automatically called on all params for tests which take
+    `codelist_json` as a parameter with indirect=True, and the output passed to
+    the test.
+    """
+    codelist = request.param  # (path, name, text, fieldnames, rows)
+    return codelist[4]
+
+
+@pytest.fixture
+def codelist_values():
+    """
+    Parses the schema for `codelist` fields and returns the values
+    (eg. 'entityType.csv') of those fields.
+    Checks all schema files at once and returns a list of all codelist values seen.
+    """
+    codelists = {}
+    schema_paths = get_schema_paths()
+    for path, name, data in schema_paths:
+        codelists.update(get_codelists_from_schema(data))
+
+    codelist_names = [name for name in codelists.keys()]
+    return codelist_names
+
+
+@pytest.fixture
+def codelist_enums(request):
+    """
+    Parses the schema for `codelist` fields and retrieves the accompanying `enum` value.
+    Expects the value of the schema `$id` field to be passed from the test,
+    and only gets values from the one schema.
+    """
+    registry = schema_registry()
+    schema_contents = registry.contents(request.param)
+    return get_codelists_from_schema(schema_contents)
+
+
+@pytest.fixture
+def get_valid_data():
+    """
+    Returns a simple valid BODS statement.
+    """
+    data = [{
+        "statementId": "abcdefgkdddddddddddddddddddddddshdkjfkjdkjf",
+        "declarationSubject": "xyz",
+        "recordId": "123",
+        "recordType": "entity",
+        "recordDetails": {
+            "entityType": "unknownEntity",
+            "isComponent": False,
+        }
+    }]
+    return data
+
+
+@pytest.fixture
+def get_invalid_data():
+    """
+    Returns a simple invalid BODS statement.
+    """
+    data = [{
+        "declarationSubject": "xyz",
+        "recordId": "123",
+        "recordType": "entity"
+    }]
+    return data
+
+
+"""
+Helper functions used by the fixtures, or sometimes the tests directly.
+"""
+
+
 def get_json_files(dir):
     """
     Recursively gets files with .json extension in `path` and returns
@@ -26,11 +192,19 @@ def get_json_files(dir):
 
 
 def file_id(path):
+    """
+    Return the file name from a path; helps with
+    understanding test errors.
+    """
     return path.name
 
 
 def codelist_id(codelist_data):
-    # codelist data is (path, name, text, fieldnames, rows) from codelist_paths
+    """
+    Return the file name of a codelist from the path data; helps with
+    understanding test errors.
+    codelist_data is (path, name, text, fieldnames, rows)
+    """
     return codelist_data[1]
 
 
@@ -142,154 +316,3 @@ def validate_metadata_presence(*args, allow_missing=_false):
         return errors
 
     return _traverse(block)(*args)
-
-
-"""
-The fixtures in this file are automatically discovered by pytest without
-needing to import them in the test files.
-"""
-
-
-@pytest.fixture
-def schema_dir():
-    return get_schema_dir()
-
-
-@pytest.fixture
-def codelists_dir():
-    return os.path.join(get_schema_dir(), 'codelists')
-
-
-@pytest.fixture
-def examples_dir():
-    return get_examples_dir()
-
-
-@pytest.fixture
-def schema_from_registry(request):
-    registry = schema_registry()
-    return registry.contents(request.param)
-
-
-@pytest.fixture
-def schema_validator():
-    """
-    This sets up and returns a 2020-12 validator, against which the BODS
-    schema can be checked. The BODS schema files are loaded into the
-    registry so $refs can be resolved.
-    """
-
-    registry = schema_registry()
-
-    # Get meta schema
-    here = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(here, 'schema', 'meta-schema.json')) as fp:
-        metaschema = json.load(fp)
-
-    validator = Draft202012Validator(metaschema, registry=registry, format_checker=FormatChecker())
-
-    return validator
-
-
-@pytest.fixture
-def bods_validator():
-    """
-    This sets up and returns the validator, using the statement.json
-    schema as the primary schema against which data is validated.
-    """
-
-    # Get the registry
-    registry = schema_registry()
-
-    # Make the validator
-    statement_schema = registry.contents("urn:statement")
-    bods_validator = Draft202012Validator(statement_schema, registry=registry, format_checker=FormatChecker())
-    return bods_validator
-
-
-@pytest.fixture
-def codelist_validator():
-    """
-    This sets up and returns a validator for the codelists schema.
-    """
-    # Get the registry
-    registry = schema_registry()
-
-    # Make the validator
-    codelist_schema = registry.contents("urn:codelists")
-    codelist_validator = Draft202012Validator(codelist_schema, registry=registry, format_checker=FormatChecker())
-    return codelist_validator
-
-
-@pytest.fixture
-def bods_json(request):
-    """
-    Returns JSON from a file path.
-    This is automatically called on all params for tests which take
-    `bods_json` as a parameter with indirect=True, and the output passed to
-    the test.
-    """
-    fp = request.param
-    bods = json.loads(fp.read_text())
-    return bods
-
-
-@pytest.fixture
-def codelist_json(request):
-    """
-    Returns JSON version of a codelist from a CSV file path.
-    This is automatically called on all params for tests which take
-    `codelist_json` as a parameter with indirect=True, and the output passed to
-    the test.
-    """
-    codelist = request.param  # (path, name, text, fieldnames, rows)
-    return codelist[4]
-
-
-@pytest.fixture
-def codelist_values():
-    codelists = {}
-    schema_paths = get_schema_paths()
-    for path, name, data in schema_paths:
-        codelists.update(get_codelists_from_schema(data))
-
-    codelist_names = [name for name in codelists.keys()]
-    return codelist_names
-
-
-@pytest.fixture
-def codelist_enums(request):
-    registry = schema_registry()
-    schema_contents = registry.contents(request.param)
-    return get_codelists_from_schema(schema_contents)
-
-
-@pytest.fixture
-def get_valid_data():
-    """
-    Returns a simple valid BODS statement.
-    """
-    data = [{
-        "statementId": "abcdefgkdddddddddddddddddddddddshdkjfkjdkjf",
-        "declarationSubject": "xyz",
-        "recordId": "123",
-        "recordType": "entity",
-        "recordDetails": {
-            "entityType": "unknownEntity",
-            "isComponent": False,
-        }
-    }]
-    return data
-
-
-@pytest.fixture
-def get_invalid_data():
-    """
-    Returns a simple invalid BODS statement.
-    """
-    data = [{
-        "declarationSubject": "xyz",
-        "recordId": "123",
-        "recordType": "entity"
-    }]
-    return data
